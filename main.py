e2e0b19802a0245ada1310f7dbd5b0e34aa11b38
OluCole <coleseyi19@gmail.com>
2024-09-26 08:18:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+first_name = input("enter your first name")
+m_i = input("enter your middle initial")
+last_name = input("enter your last name")
+s_a = input("enter street adress")
+c_z =  input("enter city and zipcode")
+ph_n = input("enter phone number")
+Email = input("enter email")
